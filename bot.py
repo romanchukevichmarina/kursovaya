a5e776929d63d764666d43329bfbe51f220fbaa8
romanchukevichmarina <romanchukevich26@icloud.com>
2024-03-28 19:12:42 +0300

    main functional

---
+++ bot.py
@@ -0,0 +1,100 @@
+import telebot
+from telebot import types
+import table_controller
+
+bot = telebot.TeleBot("6729812420:AAEOhJnCnd-kUUrfdQPHYA_UQDpHFpY0sNM")
+message_list = []
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Остатки")
+    btn2 = types.KeyboardButton("Отгрузки")
+    btn3 = types.KeyboardButton("Поступления")
+    markup.add(btn1, btn2, btn3)
+    bot.send_message(message.chat.id, text="Добрый день, {0.first_name}! Что бы вы хотели узнать?".format(message.from_user), reply_markup=markup)
+    
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if(message.text == "Остатки"):
+        message_list.append(message.text)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Общий остаток в рублях")
+        btn2 = types.KeyboardButton("Остаток определенного товара")
+        btn3 = types.KeyboardButton("Остаток контроллеров")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, btn3, back)
+        bot.send_message(message.chat.id, text="Выберите вариант", reply_markup=markup)
+    elif(message.text == "Отгрузки"):
+        message_list.append(message.text)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        week = types.KeyboardButton("За неделю")
+        month = types.KeyboardButton("За месяц")
+        markup.add(week, month)
+        bot.send_message(message.chat.id, text="За какой промежуток вывести отгрузки?", reply_markup=markup)
+    elif(message.text == "Поступления"):
+        message_list.append(message.text)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        week = types.KeyboardButton("За неделю")
+        month = types.KeyboardButton("За месяц")
+        markup.add(week, month)
+        bot.send_message(message.chat.id, text="За какой промежуток вывести поступления?", reply_markup=markup)
+    elif(message.text == "За неделю"):
+        message_list.append(message.text)
+        if message_list[-2] == "Поступления":
+            delivery = table_controller.delivery(0)
+            bot.send_message(message.chat.id, f'Поступления:\n{ delivery }')
+        elif message_list[-2] == "Отгрузки":
+            shipments = table_controller.shipments(0)
+            bot.send_message(message.chat.id, f'Отгрузки:\n{ shipments }')
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(back)
+        bot.send_message(message.chat.id, text="Вернитесь в главное меню", reply_markup=markup)
+    elif(message.text == "За месяц"):
+        message_list.append(message.text)
+        if message_list[-2] == "Поступления":
+            delivery = table_controller.delivery(1)
+            bot.send_message(message.chat.id, f'Поступления:\n{ delivery }')
+        elif message_list[-2] == "Отгрузки":
+            shipments = table_controller.shipments(1)
+            bot.send_message(message.chat.id, f'Отгрузки:\n{ shipments }')
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(back)
+        bot.send_message(message.chat.id, text="Вернитесь в главное меню", reply_markup=markup)
+    elif(message.text == "Общий остаток в рублях"):
+        message_list.append(message.text)
+        total_rest = table_controller.cash_rest()
+        bot.send_message(message.chat.id, f'Остаток товара на складе { total_rest } рублей')
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(back)
+    elif message.text == "Остаток определенного товара":
+        message_list.append(message.text)
+        bot.send_message(message.chat.id, text="Введите артикул товара")
+    elif message.text == "Остаток контроллеров":
+        message_list.append(message.text)
+        cont_rest = table_controller.rest_of_controllers()
+        bot.send_message(message.chat.id, f'Остаток контроллеров на складе:\n { cont_rest }')
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(back)
+    elif (message.text == "Вернуться в главное меню"):
+        message_list.append(message.text)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("Остатки")
+        button2 = types.KeyboardButton("Отгрузки")
+        button3 = types.KeyboardButton("Поступления")
+        markup.add(button1, button2, button3)
+        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+    else:
+        print(message_list[-1])
+        if message_list[-1] == "Остаток определенного товара":
+            id = int(message.text)
+            rest = table_controller.rest_of_good(id)
+            bot.send_message(message.chat.id, f'{rest}')
+        else:
+            bot.send_message(message.chat.id, text="Такой команды нет")
+
+bot.polling(none_stop=True)
---
+++ table_controller.py
@@ -0,0 +1,97 @@
+from datetime import datetime, timedelta
+import pandas as pd
+import numpy as np
+
+def cash_rest():
+    rest = pd.read_excel('table.xlsx', skiprows=3, index_col=1, sheet_name='Склад_остатки')
+    price = pd.read_excel('table.xlsx', index_col=1, sheet_name='справочник_товаров')
+    rest = pd.merge(rest, price, how='left', on='Наименование товара')
+    rest['total'] = rest['Стоимость'] * rest['Остаток на складе']
+    total_rest = round(rest['total'].sum())
+    return total_rest
+
+def rest_of_good(id):
+    rest = pd.read_excel('table.xlsx', skiprows=3, sheet_name='Склад_остатки')
+    price = pd.read_excel('table.xlsx', sheet_name='справочник_товаров')
+    rest = pd.merge(rest, price, how='left', on='Наименование товара')
+    rest['total'] = rest['Стоимость'] * rest['Остаток на складе']
+    res = rest.loc[rest['артикул_y'] == id]
+    if rest.empty == True:
+        s = "Нет товара с таким артикулом"
+    elif res['Остаток на складе'].empty:
+        s = "Остатоков этого товара нет на складе"
+    else:
+        s1 = f"{res['Наименование товара']}"
+        n = s1[5:(s1.find("\n"))]
+        s2 = f"{res['Остаток на складе']}"
+        count = s2[5:(s2.find("\n"))]
+        s3 = f"{res['total']}"
+        cost = s3[5:(s3.find("\n"))]
+        s = f'{n} \t{count} штук\t{cost} рублей'
+    return s
+
+def rest_of_controllers():
+    rest = pd.read_excel('table.xlsx', skiprows=3, index_col=False, sheet_name='Склад_остатки')
+    res = rest.loc[(rest['Наименование товара'].str.contains("Контроллер |контроллер "))]
+    res = res[['Наименование товара', 'Остаток на складе']]
+    res = res.groupby(['Наименование товара'], as_index=False, sort=False).agg('sum')
+    name = res['Наименование товара'].to_frame()
+    count = res['Остаток на складе'].to_frame()
+    s = ""
+    for i in range(0, name['Наименование товара'].count()):
+        if np.isnan(count['Остаток на складе'][i]) or count['Остаток на складе'][i] == 0:
+            continue
+        else:
+            s += f"{name['Наименование товара'][i]} {count['Остаток на складе'][i]} штук\n"
+    if s == "":
+        return "Нет остатков контроллеров на складе"
+    return s
+
+def shipments(n):
+    warehouse_move = pd.read_excel('table.xlsx', sheet_name='Движение_склад')
+    price = pd.read_excel('table.xlsx', sheet_name='справочник_товаров')
+    if n == 0:
+        warehouse_move = warehouse_move[ datetime.today() - pd.to_datetime(warehouse_move['Дата']) <= timedelta(days=7)]
+    elif n == 1:
+        warehouse_move = warehouse_move[warehouse_move['Дата'].dt.month == datetime.now().month]
+    warehouse_move = pd.merge(warehouse_move, price, how='left', on='Наименование товара')
+    warehouse_move['sum'] = warehouse_move['Стоимость'] * warehouse_move['Расход кол-во']
+    res = warehouse_move[['Наименование товара', 'Расход кол-во', 'sum']]
+    res = res.groupby(['Наименование товара'], as_index=False, sort=False).agg('sum')
+    name = res['Наименование товара'].to_frame()
+    count = res['Расход кол-во'].to_frame()
+    cost = res['sum'].to_frame()
+    s = ""
+    for i in range(0, name['Наименование товара'].count()):
+        if np.isnan(count['Расход кол-во'][i]) or count['Расход кол-во'][i] == 0:
+            continue
+        else:
+            s += f"{name['Наименование товара'][i]} {count['Расход кол-во'][i]} штук {cost['sum'][i]} рублей\n"
+    if s == "":
+        return "Нет отгрузок за данный период времени"
+    return s
+
+def delivery(n):
+    warehouse_move = pd.read_excel('table.xlsx', sheet_name='Движение_склад')
+    if n == 0:
+        warehouse_move = warehouse_move[ datetime.today() - pd.to_datetime(warehouse_move['Дата']) <= timedelta(days=7)]
+    elif n == 1:
+        warehouse_move = warehouse_move[warehouse_move['Дата'].dt.month == datetime.now().month]
+    price = pd.read_excel('table.xlsx', sheet_name='справочник_товаров')
+    warehouse_move = pd.merge(warehouse_move, price, how='left', on='Наименование товара')
+    warehouse_move['sum'] = warehouse_move['Стоимость'] * warehouse_move['Приход кол-во']
+    res = warehouse_move[['Наименование товара', 'Приход кол-во', 'sum']]
+    res = res.groupby(['Наименование товара'], as_index=False, sort=False).agg('sum')
+    name = res['Наименование товара'].to_frame()
+    count = res['Приход кол-во'].to_frame()
+    cost = res['sum'].to_frame()
+    s = ""
+    for i in range(0, name['Наименование товара'].count()):
+        if np.isnan(count['Приход кол-во'][i]) or count['Приход кол-во'][i] == 0:
+            continue
+        else:
+            s += f"{name['Наименование товара'][i]} {count['Приход кол-во'][i]} штук {cost['sum'][i]} рублей\n"
+    if s == "":
+        return "Нет поступлений за данный период времени"
+    return s
+print(rest_of_good(1128))
