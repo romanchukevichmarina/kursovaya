f22e8108d1d4d6bbe39f7b3055c3f8e82b18ce02
romanchukevichmarina <romanchukevich26@icloud.com>
2024-04-02 13:29:52 +0300

    change table and fix errors

--- table_controller.py
+++ table_controller.py
@@ -6,7 +6,7 @@ def cash_rest():
     rest = pd.read_excel('table.xlsx', skiprows=3, index_col=1, sheet_name='Склад_остатки')
     price = pd.read_excel('table.xlsx', index_col=1, sheet_name='справочник_товаров')
     rest = pd.merge(rest, price, how='left', on='Наименование товара')
-    rest['total'] = rest['Стоимость'] * rest['Остаток на складе']
+    rest['total'] = rest['Стоимость'] * rest['Остаток']
     total_rest = round(rest['total'].sum())
     return total_rest
 
@@ -14,16 +14,16 @@ def rest_of_good(id):
     rest = pd.read_excel('table.xlsx', skiprows=3, sheet_name='Склад_остатки')
     price = pd.read_excel('table.xlsx', sheet_name='справочник_товаров')
     rest = pd.merge(rest, price, how='left', on='Наименование товара')
-    rest['total'] = rest['Стоимость'] * rest['Остаток на складе']
+    rest['total'] = rest['Стоимость'] * rest['Остаток']
     res = rest.loc[rest['артикул_y'] == id]
     if rest.empty == True:
         s = "Нет товара с таким артикулом"
-    elif res['Остаток на складе'].empty:
+    elif res['Остаток'].empty:
         s = "Остатоков этого товара нет на складе"
     else:
         s1 = f"{res['Наименование товара']}"
         n = s1[5:(s1.find("\n"))]
-        s2 = f"{res['Остаток на складе']}"
+        s2 = f"{res['Остаток']}"
         count = s2[5:(s2.find("\n"))]
         s3 = f"{res['total']}"
         cost = s3[5:(s3.find("\n"))]
@@ -33,16 +33,16 @@ def rest_of_good(id):
 def rest_of_controllers():
     rest = pd.read_excel('table.xlsx', skiprows=3, index_col=False, sheet_name='Склад_остатки')
     res = rest.loc[(rest['Наименование товара'].str.contains("Контроллер |контроллер "))]
-    res = res[['Наименование товара', 'Остаток на складе']]
+    res = res[['Наименование товара', 'Остаток']]
     res = res.groupby(['Наименование товара'], as_index=False, sort=False).agg('sum')
     name = res['Наименование товара'].to_frame()
-    count = res['Остаток на складе'].to_frame()
+    count = res['Остаток'].to_frame()
     s = ""
     for i in range(0, name['Наименование товара'].count()):
-        if np.isnan(count['Остаток на складе'][i]) or count['Остаток на складе'][i] == 0:
+        if np.isnan(count['Остаток'][i]) or count['Остаток'][i] == 0:
             continue
         else:
-            s += f"{name['Наименование товара'][i]} {count['Остаток на складе'][i]} штук\n"
+            s += f"{name['Наименование товара'][i]} {count['Остаток'][i]} штук\n"
     if s == "":
         return "Нет остатков контроллеров на складе"
     return s
@@ -55,18 +55,18 @@ def shipments(n):
     elif n == 1:
         warehouse_move = warehouse_move[warehouse_move['Дата'].dt.month == datetime.now().month]
     warehouse_move = pd.merge(warehouse_move, price, how='left', on='Наименование товара')
-    warehouse_move['sum'] = warehouse_move['Стоимость'] * warehouse_move['Расход кол-во']
-    res = warehouse_move[['Наименование товара', 'Расход кол-во', 'sum']]
+    warehouse_move['sum'] = warehouse_move['Стоимость'] * warehouse_move['Отгрузка']
+    res = warehouse_move[['Наименование товара', 'Отгрузка', 'sum']]
     res = res.groupby(['Наименование товара'], as_index=False, sort=False).agg('sum')
     name = res['Наименование товара'].to_frame()
-    count = res['Расход кол-во'].to_frame()
+    count = res['Отгрузка'].to_frame()
     cost = res['sum'].to_frame()
     s = ""
     for i in range(0, name['Наименование товара'].count()):
-        if np.isnan(count['Расход кол-во'][i]) or count['Расход кол-во'][i] == 0:
+        if np.isnan(count['Отгрузка'][i]) or count['Отгрузка'][i] == 0:
             continue
         else:
-            s += f"{name['Наименование товара'][i]} {count['Расход кол-во'][i]} штук {cost['sum'][i]} рублей\n"
+            s += f"{name['Наименование товара'][i]} {count['Отгрузка'][i]} штук {cost['sum'][i]} рублей\n"
     if s == "":
         return "Нет отгрузок за данный период времени"
     return s
@@ -79,18 +79,18 @@ def delivery(n):
         warehouse_move = warehouse_move[warehouse_move['Дата'].dt.month == datetime.now().month]
     price = pd.read_excel('table.xlsx', sheet_name='справочник_товаров')
     warehouse_move = pd.merge(warehouse_move, price, how='left', on='Наименование товара')
-    warehouse_move['sum'] = warehouse_move['Стоимость'] * warehouse_move['Приход кол-во']
-    res = warehouse_move[['Наименование товара', 'Приход кол-во', 'sum']]
+    warehouse_move['sum'] = warehouse_move['Стоимость'] * warehouse_move['Поступление']
+    res = warehouse_move[['Наименование товара', 'Поступление', 'sum']]
     res = res.groupby(['Наименование товара'], as_index=False, sort=False).agg('sum')
     name = res['Наименование товара'].to_frame()
-    count = res['Приход кол-во'].to_frame()
+    count = res['Поступление'].to_frame()
     cost = res['sum'].to_frame()
     s = ""
     for i in range(0, name['Наименование товара'].count()):
-        if np.isnan(count['Приход кол-во'][i]) or count['Приход кол-во'][i] == 0:
+        if np.isnan(count['Поступление'][i]) or count['Поступление'][i] == 0:
             continue
         else:
-            s += f"{name['Наименование товара'][i]} {count['Приход кол-во'][i]} штук {cost['sum'][i]} рублей\n"
+            s += f"{name['Наименование товара'][i]} {count['Поступление'][i]} штук {cost['sum'][i]} рублей\n"
     if s == "":
         return "Нет поступлений за данный период времени"
     return s
